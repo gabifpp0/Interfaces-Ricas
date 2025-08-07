e57ffdd79be7577f8115a94490be3f51bde474c1
Gabrielle <143741502+gabifpp0@users.noreply.github.com>
2025-08-07 18:42:56 +0000

    feat: aplicando guard nas rotas

--- doceria/src/app/app.routes.ts
+++ doceria/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
   {
@@ -8,23 +9,32 @@ export const routes: Routes = [
     title: 'Lista de Doces'
   },
   {
+    path: 'auth/login',
+    loadComponent: () => import('./components/auth/auth.component').then(m => m.LoginComponent),
+    title: 'Login'
+  },
+  {
     path: 'doces',
     loadComponent: () => import('./components/doce-list/doce-list.component').then(m => m.DoceListComponent),
+    canActivate: [AuthGuard],
     title: 'Lista de Doces'
   },
   {
     path: 'doces/novo',
     loadComponent: () => import('./components/doce-form/doce-form.component').then(m => m.DoceFormComponent),
+    canActivate: [AuthGuard],
     title: 'Novo Doce'
   },
   {
     path: 'doces/:id',
     loadComponent: () => import('./components/doce-detail/doce-detail.component').then(m => m.DoceDetailComponent),
+    canActivate: [AuthGuard],
     title: 'Detalhes do Doce'
   },
   {
     path: 'doces/:id/editar',
     loadComponent: () => import('./components/doce-form/doce-form.component').then(m => m.DoceFormComponent),
+    canActivate: [AuthGuard],
     title: 'Editar Doce'
   },
   
